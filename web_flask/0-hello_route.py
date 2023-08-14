83e42512c28af6e3ac18e53970791dca460d097c
4mmZ <andresmaxi111014@gmail.com>
2023-08-14 13:05:11 -0700

    task 0 fix pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-
+""" document """
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def display_hello():
+    """ document """
     return "Hello HBNB!"
 
 
